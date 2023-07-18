2714501f95552521a690692ff593155840a22914
SumaiyaBK <sumiyya.fatama@gmail.com>
2023-07-18 19:47:11 +0600

    logout functionality

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -7,7 +7,8 @@ import MenuItem from "./MenuItem";
 
 import useRegisterModal from "@/app/hooks/useRegisterModal";
 import useLoginModal from "@/app/hooks/useLoginModal";
-import { User } from "next-auth";
+import { User } from "@prisma/client";
+import { signOut } from "next-auth/react";
 
 interface UserMenuProps {
   currentUser?: User | null;
@@ -95,7 +96,7 @@ const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
                 <MenuItem onClick={() => {}} label="My properties" />
                 <MenuItem onClick={() => {}} label="Airbnb my home" />
                 <hr />
-                <MenuItem onClick={() => {}} label="Logout" />
+                <MenuItem onClick={signOut} label="Logout" />
               </>
             ) : (
               <>
