cddee4697f27180f784a8a8b955cae1b1ab05ec7
SumaiyaBK <sumiyya.fatama@gmail.com>
2023-07-21 23:21:31 +0600

    pblm solve of db connection and calender

---
+++ app/actions/getListings.ts
@@ -0,0 +1,21 @@
+
+import prisma from "@/app/libs/prismadb";
+
+export default async function getListings (){
+try{
+    const listings = await prisma.listing.findMany({
+      orderBy: {
+        createdAt: 'desc'
+      }
+    })
+    const SafeListing = listings.map((listing) => ({
+      ... listing,
+      createdAt : listing.createdAt.toISOString(),
+    }));
+
+    return SafeListing;
+
+}catch(error: any){
+    throw new Error(error)
+}
+} 
